3243c6e1e9ed8f1e30f0c3fdeb9926b02e080512
Alan Chan <cf.alanpang@gmail.com>
2023-05-13 20:23:32 +0800

    123 updated

---
+++ 123.py
@@ -0,0 +1,43 @@
+# lesson 21
+
+x = 5
+
+# print(x)
+
+x = 10
+y = 20
+
+print(y+x)
+
+# declare 是創造一個給variable,是X,然後它的value 是5
+# 第一次 declare 是創造,第二次就是改變
+
+# lesson 22
+# data type 有4種
+# integer / int = 整數
+# float = 小數
+# boolean / bool = true or false
+# string / str = 'alan' 字串
+
+price = 10
+pen = 0.38
+rain = True
+name = 'alan'
+
+print (price)
+print (pen)
+print (rain)
+print (name)
+print (price, pen, rain, name, x, y)
+
+
+# lesson 23
+# input
+
+name = input ('please input your name: ')
+
+print('what the hell',name)
+
+#python3 123.py
+#cd /users/alanchan/documents/coding
+#cd /users/alanchan/desktop
